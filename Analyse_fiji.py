fba4ccadde8178a9f3ecbfd7e6428e49a9a94744
Melorob6 <merob102@ulaval.ca>
2025-04-09 10:39:48 -0400

    corrections

--- Analyse_fiji.py
+++ Analyse_fiji.py
@@ -19,14 +19,14 @@ if response.status_code != 200:
     raise ValueError(f"Échec du téléchargement : Code {response.status_code}")
 
 # Noms des axes et titre du graphique.
-x_label = "Position sur la ligne (pixels)"
+x_label = "Position (pixels)"
 y_label = "Intensité"
 title    = "Courbes d'intensité en fonction de la position (temps empilé)"
 # ================================
 
 # Charger le fichier texte contenant la matrice d'intensités.
 # Chaque ligne correspond à une image (un temps) et chaque colonne à la position sur la ligne.
-data = np.loadtxt(filename)
+data = np.loadtxt(fichier_txt)
 
 # Dimensions du tableau : nombre d'images et nombre de positions
 n_time, n_position = data.shape
